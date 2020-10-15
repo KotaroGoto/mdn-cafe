ce8b21ad2aff172b6e3c9499790e7c83a05b6d5d
Kotaro Goto <gotou-kou@mdn.co.jp>
2020-10-15 11:51:25 +0900

    1st

---
+++ components/BaseHeading.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2 class="font-sans text-lg text-gray-800 textcenter text-3xl mb-10">
+    <slot />
+  </h2>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeading',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <layout-wrapper>
+    <layout-visual
+      title="NUXT SAMPLE SITE DEMO"
+      message="おしらせやメニューをmicroCMSを導入したDEMOサイトになります。"
+    />
+    <div>
+      <div class="w-full md:max-w-3xl mx-auto pt-20 px-6 md:px-0">
+        <base-heading> MdN Cafeのおすすめメニュー </base-heading>
+        <div class="flex md:flex-wrap justify-between mb-20 md:mb-0">
+          <layout-menu-list
+            v-for="(item, index) in menuItems"
+            :key="index"
+            :image="item.image"
+            :image-url="item.image.url"
+            :name="item.name"
+            :body="item.body"
+            :price="item.price"
+            item-class="md:w-56 mb-20 shadow-lg bg-gray-200"
+            block-class="max-w"
+            image-class="w-full"
+            data-class="px-6 py-4"
+            :flag-body="false"
+          />
+        </div>
+        <div class="mb-10 mx-auto text-center">
+          <base-button name="メニューの一覧" link="/menu/" />
+        </div>
+        <base-heading> MdN Cafeのお知らせ </base-heading>
+        <div class="mb-20">
+          <layout-information-list
+            v-for="(item, index) in informationItems"
+            :id="item.id"
+            :key="index"
+            :date="item.date"
+            :title="item.title"
+          />
+        </div>
+        <div class="mb-10 mx-auto text-center">
+          <base-button name="お知らせの一覧" link="/information" />
+        </div>
+      </div>
+    </div>
+  </layout-wrapper>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData({ $config }) {
+    const menu = await axios.get(
+      `${$config.apiUrl}/menu?limit=3&filters=flag[equals]true`,
+      {
+        headers: { 'X-API-KEY': $config.apiKey },
+      }
+    )
+    const info = await axios.get(`${$config.apiUrl}/information?limit=3`, {
+      headers: { 'X-API-KEY': $config.apiKey },
+    })
+    return {
+      menuItems: menu.data.contents,
+      informationItems: info.data.contents,
+    }
+  },
+}
+</script>
+
+<style>
+.visual-home {
+  background-image: url('~@/assets/img/visual-home.jpg');
+}
+</style>
